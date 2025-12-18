7701134f2be7b66cf9c8ec111ef464f12836758b
TinyTinfoil <55459863+TinyTinfoil@users.noreply.github.com>
2025-12-17 23:48:49 -0500

    update verilog comments

--- src/digital_core.sv
+++ src/digital_core.sv
@@ -1,28 +1,8 @@
-/*  This file is part of JT49.
-
-    JT49 is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    JT49 is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with JT49.  If not, see <http://www.gnu.org/licenses/>.
-
-    Author: Jose Tejada Gomez. Twitter: @topapate
-    Version: 1.0
-    Date: 28-Jan-2019
-
-    Based on sqmusic, by the same author
-
+/*  This file is modified from JT49 to include more accurate interfaces, 
+    specifically from the jt49_bus.v file in the original repository.
     */
 
 // This is a wrapper with the BDIR/BC1 pins
-/* verilator coverage_off */
 // `timescale 1ns / 1ps
 `include "src/jt49_cen.v"
 `include "src/jt49_div.v"
@@ -45,7 +25,7 @@ module digital_core ( // note that input ports are not multiplexed
     input A8,//
     output     [7:0] dout,
     // output     [9:0] sound,  // combined channel output
-    output     [3:0] A,      // linearised channel output
+    output     [3:0] A,      // exponential channel output
     output     [3:0] B,
     output     [3:0] C,
 
